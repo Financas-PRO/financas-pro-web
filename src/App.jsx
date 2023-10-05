be5786ce5a65dd91db87d1539c0a76693c5f7b1f
joao-aglio <85963939+joao-aglio@users.noreply.github.com>
2023-10-05 09:04:07 -0300

    alterando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import EditaTurma from "./pages/editTurma/editaTurma.jsx";
 import Login from "./pages/login/login";
 import Dashboard from "./pages/dashboard/dashboard";
 import Simuladores from "./pages/simuladores/Simuladores";
-import CadastrarSimulador from "./pages/cadSimulador/cadSimulador.jsx";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Importa from "./pages/importarAluno/importa";
 import Empresa from "./pages/empresa/empresa";
@@ -19,33 +18,32 @@ import CadSimulador02 from "./pages/cadSimulador02/cadSimulador02";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-          <Route path="/" element={<Dashboard />} />   
-               
-          <Route element={<ProtectedRoute route="scopeAluno"/>}>
-            <Route path="/simuladores/:id/" element={<Simuladores />} />
-            <Route path="/empresa" element={<Empresa />} />
-            <Route path="/turmas" element={<Turmas />} />
-            <Route path="/simulador/cadastrar/:id/" element={<CadastrarSimulador />} />
-            <Route path="/simulador02/cadastrar/:id/" element={<CadSimulador02 />} />
-          </Route>   
+        <Route path="/" element={<Dashboard />} />
 
-          <Route element={<ProtectedRoute route="scopeDoc"/>}>
-            <Route path="/importa/:id/" element={<Importa />} />
-          </Route>
+        <Route element={<ProtectedRoute route="scopeAluno" />}>
+          <Route path="/simuladores/:id/" element={<Simuladores />} />
+          <Route path="/empresa" element={<Empresa />} />
+          <Route path="/simulador02/cadastrar/:id/" element={<CadSimulador02 />} />
+        </Route>
 
-            <Route path="turma/cadastrar" element={<CadastrarTurma />} />
-            <Route path="turma/gerenciar" element={<GerenciarTurma />} />
-            <Route path="turma/:id/editar" element={<EditaTurma />} />
-            <Route path="professor/cadastrar" element={<CadastrarProfessor />} />
-            <Route path="professor/gerenciar" element={<GerenciarProfessor />} />
-            <Route path="professor/:id/editar" element={<EditaProfessor />} />
+        <Route element={<ProtectedRoute route="scopeDoc" />}>
+          <Route path="/importa/:id/" element={<Importa />} />
+        </Route>
 
-        </Routes>
-      </BrowserRouter>
+        <Route path="turma/cadastrar" element={<CadastrarTurma />} />
+        <Route path="turma/gerenciar" element={<GerenciarTurma />} />
+        <Route path="turma/:id/editar" element={<EditaTurma />} />
+        <Route path="professor/cadastrar" element={<CadastrarProfessor />} />
+        <Route path="professor/gerenciar" element={<GerenciarProfessor />} />
+        <Route path="professor/:id/editar" element={<EditaProfessor />} />
+        <Route path="/turmas" element={<Turmas />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
