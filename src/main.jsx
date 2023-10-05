77ca407e4f8ba89f8f2022a1e2c27dfb264cd14b
joao-aglio <85963939+joao-aglio@users.noreply.github.com>
2023-10-05 08:16:25 -0300

    corrigindo outros conflitos

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-<<<<<<< HEAD
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -18,5 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App/>
   </>,
->>>>>>> front
 )
