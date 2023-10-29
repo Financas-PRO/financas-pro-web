1afe35f087cd326fa9406e066db7c4b88a958d7d
João Paulo Pontes <76744287+joppo75@users.noreply.github.com>
2023-10-29 11:18:29 -0300

    Alteração da pagina de erro

--- src/pages/erro/erro.jsx
+++ src/pages/erro/erro.jsx
@@ -1,11 +1,22 @@
 import "./erro.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../../components/navbar/header.jsx";
+import img from "../../assets/image/erro.png";
 
 
-export default function Erro(){
-    return(
-        <div>
-            
+export default function Erro() {
+    return (
+        <div className="row-page">
+
+            <div className="col col-md-2 col-2">
+                <Header />
+            </div>
+
+            <div className="container mt-4 col-md-8 col-9">
+
+                <img src={img} alt="" />
+                
+            </div>
         </div>
     );
 }
