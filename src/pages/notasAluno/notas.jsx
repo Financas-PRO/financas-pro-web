c2031aaafd9e1b0df8ce06f17fbd81db94b91461
João Paulo Pontes <76744287+joppo75@users.noreply.github.com>
2023-11-14 19:18:32 -0300

    Alterações para trazer as turmas na notas

--- src/pages/notasAluno/notas.jsx
+++ src/pages/notasAluno/notas.jsx
@@ -14,38 +14,33 @@ import { useParams } from "react-router-dom";
 
 export default function Notas() {
 
-  
-        const [classes, setClasses] = useState([]);
-        const [selectedClass, setSelectedClass] = useState(null);
+    const [classes, setClasses] = useState([]);
+    const [selectedClass, setSelectedClass] = useState(null);
 
-        useEffect(() => {
-            // Função para carregar as classes da API
-            const fetchClasses = async () => {
-                try {
-                    const response = await fetch('https://financaspro.cloud/api/turma/'); // Substitua pela URL da sua API
-                    const data = await response.json();
+    let { id } = useParams();
 
-                    // Verifica se o retorno da API é um array antes de atualizar o estado
-                    if (Array.isArray(data)) {
-                        setClasses(data);
-                    } else {
-                        console.error('A API não retornou um array:', data);
-                    }
-                } catch (error) {
-                    console.error('Erro ao carregar as turmas:', error);
+    useEffect(() => {
+        
+            api.get(`turma`).then((res) => {
+                const data = res.data.data;
+              
+                if (Array.isArray(data)) {
+                    setClasses(data);
+                } else {
+                    console.error('A API não retornou um array:', data);
                 }
-            };
+            })
+            .catch((error) => {
+                console.error('Erro ao carregar as turmas:', error);
+            });
+        
+    }, []);
 
-            // Chama a função de carregamento
-            fetchClasses();
-        }, []);
-
-        const handleClassChange = (e) => {
-            const selectedClassId = parseInt(e.target.value, 10);
-            const selectedClass = classes.find((cls) => cls.id === selectedClassId);
-            setSelectedClass(selectedClass);
-        };
-    
+    const handleTurmaChange = (e) => {
+        const selectedClassId = parseInt(e.target.value);
+        const selectedClass = classes.find((cls) => cls.id === selectedClassId);
+        setSelectedClass(selectedClass);
+    };
 
     return (
         <div className="row-page">
@@ -63,38 +58,28 @@ export default function Notas() {
                 <div className="row mt-5">
                     <div className="col col-md-12 col-12">
                         <label className="form-label labelselect"><i class="bi bi-clipboard-plus"></i>Selecione uma turma</label>
-                        {/* <select class="form-select" aria-label="Default select example">
-                            <option selected>Selecione</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select> */}
 
 
 
                         <div>
-                            <h1>Seletor de Turmas</h1>
-                            <select onChange={handleClassChange}>
+
+                            <select onChange={handleTurmaChange}>
                                 <option value="">Selecione uma turma</option>
-                                {Array.isArray(classes) &&
-                                    classes.map((cls) => (
-                                        <option key={cls.id} value={cls.id}>
-                                            {cls.descricao}
-                                        </option>
-                                    ))}
+                                {classes.map((cls) => (
+                                    <option key={cls.id} value={cls.id}>
+                                        {cls.descricao}
+                                    </option>
+                                ))}
                             </select>
 
                             {selectedClass && (
                                 <div>
                                     <h2>Informações da Turma Selecionada</h2>
-                                    <p>Nome da Turma: {selectedClass.descricao}</p>
-                                    
+                                    <p>Descrição da Turma: {selectedClass.descricao}</p>
+                                   
                                 </div>
                             )}
                         </div>
-
-
-
 
 
                     </div>
