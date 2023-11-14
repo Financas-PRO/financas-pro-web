4dbdeec2109e58e235a895999a653625f25f2bcc
eadEstagio2 <joaolucas123.ba@gmail.com>
2023-11-14 13:54:32 -0300

    videos dinamicos + dashboard

--- src/components/videos/Video.jsx
+++ src/components/videos/Video.jsx
@@ -5,10 +5,9 @@ const Video = props => {
     return (
         
             <button className="card-video rounded-3" onClick={props.onClick}>
-                <div className="card-img" style={{backgroundImage: `url("${props.thumbnail}")`}}></div>
+                <div className="card-img" style={{backgroundImage: `url("https://img.youtube.com/vi/${props.codigo}/maxresdefault.jpg")`}}></div>
                 <div className="card-conteudo mt-3">
                     <span className="conteudo-titulo">{props.titulo}</span>
-                    <span>{props.descricao}</span>
                     <div className="button-assita mt-3">
                         <span className="btn-assistir">CLIQUE PARA ASSISTIR</span>
                     </div>
